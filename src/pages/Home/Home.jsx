ff6197ea531f675a8195d474518e4cc68774b21c
Soundlover1984 <denys.kovtun@gmail.com>
2023-07-29 19:26:09 +0300

    Home and MovieList added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,36 @@
+import { fetchMovieApi } from "movieApiService";
+import { useState, useEffect } from "react";
+import { Title, Main } from './Home.styled';
+import { MovieList } from '../../components/MovieList/MovieList';
+import { Btn } from '../../components/Button/Button';
+
+export const Home = () => {
+    const [movies, setMovies] = useState([]);
+
+    const [page, setPage] = useState(1);
+  
+    const [total_results, setTotalResults] = useState(null);
+
+    useEffect(() => {
+        fetchMovieApi.getPopularMovies(page).then(data => {
+          setMovies(movies => [...movies, ...data.results]);
+          setTotalResults(data.total_results);
+        });
+      }, [page]);
+
+      const loadMore = () => {
+        setPage(page => page + 1);
+      };
+    
+    
+  return (
+    <Main>
+      <Title>Trending today </Title>
+      <MovieList movies={movies} />
+      {total_results / 20 >= page && (
+        <Btn text="Load more" status="load" onClick={loadMore} />
+      )}
+    </Main>
+  );
+};
+  
